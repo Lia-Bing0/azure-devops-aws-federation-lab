375ad078daca5f68dca7b7c0c826eceab35e71a9
Lia Chandler <ktchandler92@gmail.com>
2026-03-13 17:51:06 -0400

    Add intentionally insecure security group for Checkov validation
    - added SSH security group allowing 0.0.0.0/0
    - attached security group to EC2 instance
    - used to demonstrate DevSecOps security scanning failure

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -3,7 +3,36 @@ resource "aws_instance" "devsecops_lab" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t3.micro"
 
+  vpc_security_group_ids = [
+    aws_security_group.insecure_ssh.id
+  ]
+
   tags = {
     Name = "devsecops-federation-lab"
+  }
+}
+
+resource "aws_security_group" "insecure_ssh" {
+
+  name        = "devsecops-lab-insecure-sg"
+  description = "Intentional insecure security group for Checkov demonstration"
+
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devsecops-insecure-sg"
   }
 }
